5b56853648d4de0828386f1a0bfc824be9fb65bf
nacht <124697363+nachtalia@users.noreply.github.com>
2024-09-24 21:07:33 +0800

    dashboad & modal

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,22 +1,52 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import { ref } from 'vue'
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
+import { Head } from '@inertiajs/vue3'
+import BarChart from '@/Components/ui/chart/BarChart.vue'
 
-const activeSection = ref('sales');
+const activeSection = ref('sales')
 </script>
 
 <template>
-  <Head title="Products" />
+   <Head title="Products" />
 
-  <AuthenticatedLayout>
-    <template #header>
-      <h2 class="font-sans text-3xl antialiased font-bold leading-tight text-gray-800">Dashboard</h2>
-    </template>
+   <AuthenticatedLayout>
+      <template #header>
+         <h2 class="font-sans text-3xl antialiased font-bold leading-tight text-gray-800">
+            Dashboard
+         </h2>
+      </template>
 
-    <div class="py-1 overflow-hidden bg-white border border-gray-200 rounded-xl">
-        DAshboard
-</div>
+      <div class="flex">
+         <div class="flex-auto w-80 bg-white border border-gray-200 rounded p-4 me-4 font-semibold">
+            Total Revenue
+            <div class="font-bold text-7xl mt-2">$1,213.00</div>
+         </div>
+         <div class="flex-auto w-20 bg-white border border-gray-200 rounded p-4 font-semibold">
+            Products
+            <div class="font-bold text-7xl mt-2">54,321</div>
+         </div>
+      </div>
 
-  </AuthenticatedLayout>
+      <div class="border border-gray-200 bg-white rounded mt-4">
+         <div class="flex">
+            <div class="flex-auto w-80 border border-gray-200 p-4 font-semibold">
+               Bar Chart - Interactive
+               <div class="font-normal">Showing total visitors for the last 3 months</div>
+            </div>
+            <div class="flex-auto w-10 border border-gray-200 p-4">
+               Desktop
+               <div class="font-bold text-3xl">24,828</div>
+            </div>
+            <div class="flex-auto w-10 border border-gray-200 p-4">
+               Desktop
+               <div class="font-bold text-3xl">25,010</div>
+            </div>
+         </div>
+
+         <div>
+            <BarChart />
+         </div>
+      </div>
+   </AuthenticatedLayout>
 </template>
--- resources/js/Pages/Inventory.vue
+++ resources/js/Pages/Inventory.vue
@@ -1,122 +1,278 @@
 <script setup>
-import { ref, computed } from 'vue';
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import { ref, computed } from 'vue'
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue'
+import { Head } from '@inertiajs/vue3'
 
-import Pagination from '@/Components/ui/pagination/Pagination.vue';
-import PaginationEllipsis from '@/Components/ui/pagination/PaginationEllipsis.vue';
-import PaginationFirst from '@/Components/ui/pagination/PaginationFirst.vue';
-import PaginationLast from '@/Components/ui/pagination/PaginationLast.vue';
-import PaginationNext from '@/Components/ui/pagination/PaginationNext.vue';
-import PaginationPrev from '@/Components/ui/pagination/PaginationPrev.vue';
+import Modal from '@/Components/Modal.vue'
 
-import Button from '@/Components/ui/button/Button.vue';
-import Table from '@/Components/ui/table/Table.vue';
-import TableBody from '@/Components/ui/table/TableBody.vue';
-import TableCaption from '@/Components/ui/table/TableCaption.vue';
-import TableCell from '@/Components/ui/table/TableCell.vue';
-import TableHead from '@/Components/ui/table/TableHead.vue';
-import TableHeader from '@/Components/ui/table/TableHeader.vue';
-import TableRow from '@/Components/ui/table/TableRow.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
+import Pagination from '@/Components/ui/pagination/Pagination.vue'
+import PaginationEllipsis from '@/Components/ui/pagination/PaginationEllipsis.vue'
+import PaginationFirst from '@/Components/ui/pagination/PaginationFirst.vue'
+import PaginationLast from '@/Components/ui/pagination/PaginationLast.vue'
+import PaginationNext from '@/Components/ui/pagination/PaginationNext.vue'
+import PaginationPrev from '@/Components/ui/pagination/PaginationPrev.vue'
+
+import Button from '@/Components/ui/button/Button.vue'
+import Table from '@/Components/ui/table/Table.vue'
+import TableBody from '@/Components/ui/table/TableBody.vue'
+import TableCaption from '@/Components/ui/table/TableCaption.vue'
+import TableCell from '@/Components/ui/table/TableCell.vue'
+import TableHead from '@/Components/ui/table/TableHead.vue'
+import TableHeader from '@/Components/ui/table/TableHeader.vue'
+import TableRow from '@/Components/ui/table/TableRow.vue'
+import PrimaryButton from '@/Components/PrimaryButton.vue'
 
 const invoices = ref([
-  { id: 'INV001', description: 'Paid', price: '$250.00', stock: 'Credit Card', category: 'Badge' },
-  { id: 'INV002', description: 'Pending', price: '$150.00', stock: 'PayPal', category: 'Badge' },
-  { id: 'INV003', description: 'Unpaid', price: '$350.00', stock: 'Bank Transfer', category: 'Badge' },
-  { id: 'INV004', description: 'Paid', price: '$200.00', stock: 'Credit Card', category: 'Badge' },
-  { id: 'INV005', description: 'Refunded', price: '$100.00', stock: 'Credit Card', category: 'Badge' },
-  { id: 'INV006', description: 'Refunded', price: '$100.00', stock: 'Credit Card', category: 'Badge' },
-  { id: 'INV007', description: 'Refunded', price: '$100.00', stock: 'Credit Card', category: 'Badge' },
-  { id: 'INV008', description: 'Refunded', price: '$100.00', stock: 'Credit Card', category: 'Badge' },
-  { id: 'INV009', description: 'Refunded', price: '$100.00', stock: 'Credit Card', category: 'Badge' },
-  { id: 'INV010', description: 'Refunded', price: '$100.00', stock: 'Credit Card', category: 'Badge' },
-  { id: 'INV011', description: 'Refunded', price: '$100.00', stock: 'Credit Card', category: 'Badge' },
-  { id: 'INV012', description: 'Refunded', price: '$100.00', stock: 'Credit Card', category: 'Badge' },
-  { id: 'INV013', description: 'Refunded', price: '$100.00', stock: 'Credit Card', category: 'Badge' },
-  { id: 'INV014', description: 'Refunded', price: '$100.00', stock: 'Credit Card', category: 'Badge' },
-]);
+   { id: 'INV001', description: 'Paid', price: '$250.00', stock: 'Credit Card', category: 'Badge' },
+   { id: 'INV002', description: 'Pending', price: '$150.00', stock: 'PayPal', category: 'Badge' },
+   {
+      id: 'INV003',
+      description: 'Unpaid',
+      price: '$350.00',
+      stock: 'Bank Transfer',
+      category: 'Badge'
+   },
+   { id: 'INV004', description: 'Paid', price: '$200.00', stock: 'Credit Card', category: 'Badge' },
+   {
+      id: 'INV005',
+      description: 'Refunded',
+      price: '$100.00',
+      stock: 'Credit Card',
+      category: 'Badge'
+   },
+   {
+      id: 'INV006',
+      description: 'Refunded',
+      price: '$100.00',
+      stock: 'Credit Card',
+      category: 'Badge'
+   },
+   {
+      id: 'INV007',
+      description: 'Refunded',
+      price: '$100.00',
+      stock: 'Credit Card',
+      category: 'Badge'
+   },
+   {
+      id: 'INV008',
+      description: 'Refunded',
+      price: '$100.00',
+      stock: 'Credit Card',
+      category: 'Badge'
+   },
+   {
+      id: 'INV009',
+      description: 'Refunded',
+      price: '$100.00',
+      stock: 'Credit Card',
+      category: 'Badge'
+   },
+   {
+      id: 'INV010',
+      description: 'Refunded',
+      price: '$100.00',
+      stock: 'Credit Card',
+      category: 'Badge'
+   },
+   {
+      id: 'INV011',
+      description: 'Refunded',
+      price: '$100.00',
+      stock: 'Credit Card',
+      category: 'Badge'
+   },
+   {
+      id: 'INV012',
+      description: 'Refunded',
+      price: '$100.00',
+      stock: 'Credit Card',
+      category: 'Badge'
+   },
+   {
+      id: 'INV013',
+      description: 'Refunded',
+      price: '$100.00',
+      stock: 'Credit Card',
+      category: 'Badge'
+   },
+   {
+      id: 'INV014',
+      description: 'Refunded',
+      price: '$100.00',
+      stock: 'Credit Card',
+      category: 'Badge'
+   }
+])
 
-const currentPage = ref(1);
-const pageSize = 5;
+const currentPage = ref(1)
+const pageSize = 5
+const showModal = ref(false)
 
 const paginatedInvoices = computed(() => {
-  const start = (currentPage.value - 1) * pageSize;
-  return invoices.value.slice(start, start + pageSize);
-});
+   const start = (currentPage.value - 1) * pageSize
+   return invoices.value.slice(start, start + pageSize)
+})
 
 const handlePageChange = (page) => {
-  currentPage.value = page;
-};
+   currentPage.value = page
+}
+
+const openModal = () => {
+   showModal.value = true
+}
+
+const closeModal = () => {
+   showModal.value = false
+}
 </script>
 
-
 <template>
-    <Head title="Products" />
-  
-    <AuthenticatedLayout>
-      <template #header>
-        <h2 class="font-sans text-3xl antialiased font-bold leading-tight text-gray-800">Products</h2>
-      </template>
-  
-      <div class="flex flex-row-reverse mb-6">
-        <PrimaryButton>
-          <span>
-            <svg class="w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
-            </svg>
-          </span>
-          Create
-        </PrimaryButton>
-      </div>
-  
-      <div class="py-1 overflow-hidden bg-white border border-gray-200 rounded-xl">
-        <Table>
-    <!-- Table Headers -->
-    <TableHeader>
-      <TableRow>
-        <TableHead>ID</TableHead>
-        <TableHead>Description</TableHead>
-        <TableHead>Price</TableHead>
-        <TableHead>Stock</TableHead>
-        <TableHead>Category</TableHead>
-      </TableRow>
-    </TableHeader>
+   <Head title="Products" />
 
-    <!-- Table Body -->
-    <TableBody>
-      <TableRow v-for="invoice in paginatedInvoices" :key="invoice.id">
-        <TableCell>{{ invoice.id }}</TableCell>
-        <TableCell>{{ invoice.description }}</TableCell>
-        <TableCell>{{ invoice.price }}</TableCell>
-        <TableCell>{{ invoice.stock }}</TableCell>
-        <TableCell>
-          <span class="p-1 bg-gray-200 border rounded-full pe-2 ps-2">{{ invoice.category }}</span>
-        </TableCell>
-      </TableRow>
-    </TableBody>
-  </Table>
+   <AuthenticatedLayout>
+      <template #header>
+         <h2 class="font-sans text-3xl antialiased font-bold leading-tight text-gray-800">
+            Products
+         </h2>
+      </template>
+
+      <div class="flex flex-row-reverse mb-6">
+         <PrimaryButton @click="openModal">
+            <span>
+               <svg
+                  class="w-6 h-6 text-gray-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+               >
+                  <path
+                     stroke-linecap="round"
+                     stroke-linejoin="round"
+                     stroke-width="2"
+                     d="M12 4v16m8-8H4"
+                  />
+               </svg>
+            </span>
+            Create
+         </PrimaryButton>
       </div>
-  
+
+      <div class="py-1 overflow-hidden bg-white border border-gray-200 rounded-xl">
+         <Table>
+            <TableHeader>
+               <TableRow>
+                  <TableHead>ID</TableHead>
+                  <TableHead>Description</TableHead>
+                  <TableHead>Price</TableHead>
+                  <TableHead>Stock</TableHead>
+                  <TableHead>Category</TableHead>
+               </TableRow>
+            </TableHeader>
+
+            <TableBody>
+               <TableRow v-for="invoice in paginatedInvoices" :key="invoice.id">
+                  <TableCell>{{ invoice.id }}</TableCell>
+                  <TableCell>{{ invoice.description }}</TableCell>
+                  <TableCell>{{ invoice.price }}</TableCell>
+                  <TableCell>{{ invoice.stock }}</TableCell>
+                  <TableCell>
+                     <span class="p-1 bg-gray-200 border rounded-full pe-2 ps-2">{{
+                        invoice.category
+                     }}</span>
+                  </TableCell>
+               </TableRow>
+            </TableBody>
+         </Table>
+      </div>
+
       <!-- Pagination Controls -->
       <div class="flex items-center justify-between mt-4">
-        <Pagination :total="invoices.length" :sibling-count="1" :default-page="currentPage" @page-changed="handlePageChange">
-          <template v-slot:pagination-controls="{ page, items }">
-            <PaginationFirst :current="page" />
-            <PaginationPrev :current="page" />
-            <div class="flex items-center gap-1">
-              <template v-for="(item, index) in items" :key="index">
-                <PaginationEllipsis v-if="item.type === 'ellipsis'" :index="index" />
-                <Button v-else-if="item.type === 'page'" @click="handlePageChange(item.value)" :class="{ 'bg-blue-500 text-white': item.value === page }">
-                  {{ item.value }}
-                </Button>
-              </template>
-            </div>
-            <PaginationNext :current="page" />
-            <PaginationLast :current="page" />
-          </template>
-        </Pagination>
+         <Pagination
+            :total="invoices.length"
+            :sibling-count="1"
+            :default-page="currentPage"
+            @page-changed="handlePageChange"
+         >
+            <template v-slot:pagination-controls="{ page, items }">
+               <PaginationFirst :current="page" />
+               <PaginationPrev :current="page" />
+               <div class="flex items-center gap-1">
+                  <template v-for="(item, index) in items" :key="index">
+                     <PaginationEllipsis v-if="item.type === 'ellipsis'" :index="index" />
+                     <Button
+                        v-else-if="item.type === 'page'"
+                        @click="handlePageChange(item.value)"
+                        :class="{ 'bg-blue-500 text-white': item.value === page }"
+                     >
+                        {{ item.value }}
+                     </Button>
+                  </template>
+               </div>
+               <PaginationNext :current="page" />
+               <PaginationLast :current="page" />
+            </template>
+         </Pagination>
       </div>
-    </AuthenticatedLayout>
-  </template>
-  
+
+      <!-- Modal Component -->
+      <Modal :show="showModal" @close="closeModal">
+         <template #default>
+            <h3 class="text-lg font-semibold p-4">Create New Invoice</h3>
+            <form class="p-4">
+               <div class="mt-4">
+                  <label for="description" class="block text-sm font-medium text-gray-700"
+                     >Description</label
+                  >
+                  <input
+                     id="description"
+                     type="text"
+                     class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-500"
+                  />
+               </div>
+               <div class="mt-4">
+                  <label for="price" class="block text-sm font-medium text-gray-700">Price</label>
+                  <input
+                     id="price"
+                     type="text"
+                     class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-500"
+                  />
+               </div>
+               <div class="mt-4">
+                  <label for="stock" class="block text-sm font-medium text-gray-700">Stock</label>
+                  <input
+                     id="stock"
+                     type="text"
+                     class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-500"
+                  />
+               </div>
+               <div class="mt-4">
+                  <label for="category" class="block text-sm font-medium text-gray-700"
+                     >Category</label
+                  >
+                  <input
+                     id="category"
+                     type="text"
+                     class="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring focus:ring-blue-500"
+                  />
+               </div>
+               <div class="mt-4">
+                  <button
+                     type="button"
+                     class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br
+                        focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg
+                        text-sm px-5 py-2.5 text-center me-2 mb-2"
+                  >
+                     Submit
+                  </button>
+               </div>
+            </form>
+         </template>
+         <template #footer>
+            <Button @click="closeModal">Cancel</Button>
+            <PrimaryButton @click="closeModal">Create</PrimaryButton>
+         </template>
+      </Modal>
+   </AuthenticatedLayout>
+</template>
