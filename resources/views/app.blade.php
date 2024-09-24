70b196842f2ee8b3627ebdbe753d697c02c49bce
Edward Gulmayo <astaltin@gmail.com>
2024-09-25 06:27:38 +0800

    refactor: remove !important flag from font-family rule in 'app.css'

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,7 +11,7 @@
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body class="antialiased">
         @inertia
     </body>
 </html>
