1cf54c61df07d7af31d68c36574ebf0fad27f902
Edward Gulmayo <astaltin@gmail.com>
2024-09-25 06:18:17 +0800

    refactor: remove some elements; redesign and re-organize the 'sidebar' component

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,128 +1,91 @@
 <script setup>
-import { ref } from "vue";
-import ApplicationLogo from "@/Components/ApplicationLogo.vue";
-import Dropdown from "@/Components/Dropdown.vue";
-import { ArrowRightIcon, ShoppingCartIcon, UsersIcon, CogIcon } from '@heroicons/vue/24/outline';
-import DropdownLink from "@/Components/DropdownLink.vue";
-import NavLink from "@/Components/NavLink.vue";
-import { Link } from "@inertiajs/vue3";
+import ApplicationLogo from '@/Components/ApplicationLogo.vue'
+import NavLink from '@/Components/NavLink.vue'
+import { buttonVariants } from '@/Components/ui/button'
+import {
+   DropdownMenu,
+   DropdownMenuContent,
+   DropdownMenuItem,
+   DropdownMenuLabel,
+   DropdownMenuSeparator,
+   DropdownMenuTrigger
+} from '@/Components/ui/dropdown-menu'
+import { Separator } from '@/Components/ui/separator'
+import { cn } from '@/lib/utils'
+import { Link } from '@inertiajs/vue3'
+import { ChevronDownIcon, GaugeIcon, PackageIcon } from 'lucide-vue-next'
 
-// State for controlling sidebar visibility
-const showingSidebar = ref(true);
+const ICON_SIZE = 'size-5'
+
+const navRoutes = [
+   { href: 'dashboard', icon: GaugeIcon, title: 'Dashboard' },
+   { href: 'inventory', icon: PackageIcon, title: 'Inventory' }
+]
 </script>
 
 <template>
-   <div class="flex min-h-screen bg-gray-100">
-     <!-- Sidebar -->
-     <aside
-       :class="{ hidden: !showingSidebar }"
-       class="fixed top-0 left-0 w-64 h-screen bg-white border-r border-gray-200"
-     >
-       <div class="flex flex-col h-full">
-         <div class="flex-1 px-6 py-4">
-           <div class="flex items-center">
-             <ApplicationLogo class="block w-auto text-gray-800 fill-current h-9" />
-           </div>
- 
-           <nav class="mt-6">
-             <ul>
-               <li>
-                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                   <ArrowRightIcon class="w-5 h-5 text-gray-500" />
-                   <span class="ms-3">Products</span>
-                 </NavLink>
-               </li>
-               <li>
-                 <NavLink :href="route('inventory')" :active="route().current('inventory')">
-                   <ShoppingCartIcon class="w-5 h-5 text-gray-500" />
-                   <span class="ms-3">Inventory</span>
-                 </NavLink>
-               </li>
-               <li>
-                 <NavLink :href="route('user')" :active="route().current('user')">
-                   <UsersIcon class="w-5 h-5 text-gray-500" />
-                   <span class="ms-3">Users</span>
-                 </NavLink>
-               </li>
-               <li>
-                 <NavLink :href="route('setting')" :active="route().current('setting')">
-                   <CogIcon class="w-5 h-5 text-gray-500" />
-                   <span class="ms-3">Settings</span>
-                 </NavLink>
-               </li>
-             </ul>
-           </nav>
+   <div class="grid grid-cols-[15.625em_1fr]">
+      <!-- SIDEBAR -->
+      <aside class="border flex flex-col min-h-svh">
+         <!-- SIDEBAR HEADER -->
+         <div class="inline-flex gap-1.5 justify-center items-center px-[0.875em] py-2">
+            <span class="pt-2 pb-5">
+               <ApplicationLogo class="size-16" />
+            </span>
          </div>
- 
-         <!-- Dropdown Menu -->
-         <div class="px-6 py-4 mt-auto border-t border-gray-200">
-           <Dropdown align="bottom" width="48">
-             <template #trigger>
-               <span class="inline-flex rounded-md">
-                 <button
-                   type="button"
-                   class="inline-flex items-center px-3 py-2 text-sm font-bold leading-4 text-gray-500 transition duration-150 ease-in-out bg-white border border-transparent rounded-md hover:text-gray-700 focus:outline-none text-start"
-                 >
-                   <div>
-                     <span class="text-sm font-bold text-gray-600">
-                       {{ $page.props.auth.user.name }}
-                     </span>
-                     <br class="my-2" />
-                     <span class="text-xs text-gray-500">
-                       {{ $page.props.auth.user.email }}
-                     </span>
-                   </div>
-                   <svg
-                     class="ms-2 -me-0.5 h-4 w-4"
-                     xmlns="http://www.w3.org/2000/svg"
-                     viewBox="0 0 20 20"
-                     fill="currentColor"
-                   >
-                     <path
-                       fill-rule="evenodd"
-                       d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                       clip-rule="evenodd"
-                     />
-                   </svg>
-                 </button>
-               </span>
-             </template>
- 
-             <template #content>
-               <div class="relative">
-                 <div
-                   class="absolute right-0 z-10 w-48 mb-2 bg-white border border-gray-200 rounded-md shadow-lg bottom-full"
-                 >
-                   <DropdownLink :href="route('profile.edit')">
-                     Profile
-                   </DropdownLink>
-                   <DropdownLink
-                     :href="route('logout')"
-                     method="post"
-                     as="button"
-                   >
-                     Log Out
-                   </DropdownLink>
-                 </div>
-               </div>
-             </template>
-           </Dropdown>
-         </div>
-       </div>
-     </aside>
- 
-     <!-- Main content -->
-     <div class="flex-1 ml-64">
-       <header class="bg-white shadow" v-if="$slots.header">
-         <div class="px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
-           <slot name="header" />
-         </div>
-       </header>
- 
-       <!-- Page Content -->
-       <main class="p-4">
+         <!-- SIDEBAR NAV -->
+         <nav class="grow flex flex-col gap-2 p-2">
+            <template v-for="navRoute in navRoutes">
+               <NavLink
+                  :class="
+                     cn(
+                        buttonVariants({ variant: 'ghost' }),
+                        'gap-1.5 justify-start p-1.5 text-base'
+                     )
+                  "
+                  :href="route(navRoute.href)"
+                  :active="route().current(navRoute.href)"
+               >
+                  <component :class="ICON_SIZE" :is="navRoute.icon" />
+                  <span class="font-medium">{{ navRoute.title }}</span>
+               </NavLink>
+            </template>
+         </nav>
+         <Separator />
+         <!-- SIDEBAR FOOTER -->
+         <section class="grid grid-cols-[1fr_1.25em] px-[0.875em] py-2">
+            <p class="font-medium text-sm">{{ $page.props.auth.user.name }}</p>
+            <p class="text-xs text-muted-foreground">
+               {{ $page.props.auth.user.email }}
+            </p>
+            <div class="row-span-full col-start-2 h-5">
+               <!-- SIDEBAR MENU -->
+               <DropdownMenu>
+                  <DropdownMenuTrigger
+                     :class="cn(buttonVariants({ size: 'icon', variant: 'ghost' }), ICON_SIZE)"
+                  >
+                     <ChevronDownIcon :class="ICON_SIZE" />
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                     <DropdownMenuLabel>Menu</DropdownMenuLabel>
+                     <DropdownMenuSeparator />
+                     <DropdownMenuItem class="px-0 py-0">
+                        <Link
+                           class="grow px-2 py-1.5 text-start"
+                           method="post"
+                           as="button"
+                           :href="route('logout')"
+                           >Logout</Link
+                        >
+                     </DropdownMenuItem>
+                  </DropdownMenuContent>
+               </DropdownMenu>
+            </div>
+         </section>
+      </aside>
+      <!-- MAIN CONTENT  -->
+      <main class="p-2">
          <slot />
-       </main>
-     </div>
+      </main>
    </div>
- </template>
+</template>
